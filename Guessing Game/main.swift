c480d19eb3ac3221a241c8be2542e72e171946ef
Jake Dillon <jakedillon@macbook-pro-60.local>
2018-08-16 10:22:43 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Jake Dillon on 8/16/18.
+//  Copyright © 2018 Jake Dillon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
